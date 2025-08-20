282b653123d223ef1c07d8397078f9d1b6c1863c
bianca-isbly <biancaisabely7@gmail.com>
2025-08-20 14:58:51 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("Light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a img
+  if (html.classList.contains("Light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    // >
+    //
+    // Tem que trocar a imagem do light ainda BIA!
+    //
+    //
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
+
+
